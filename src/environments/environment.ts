f97709b6af8c79e2ec2c92d2484e0f5ac4e13f18
Oluwanishola12 <taiwo5801@gmail.com>
2023-11-28 19:10:03 +0100

    realones

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'oluwanishola-650e9',
+    appId: '1:809552210940:web:60547c8d6b55a961962079',
+    storageBucket: 'oluwanishola-650e9.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDwVuzH81bUoAkrhGj1uHOjjipelRoIHzE',
+    authDomain: 'oluwanishola-650e9.firebaseapp.com',
+    messagingSenderId: '809552210940',
+    measurementId: 'G-M9WTKEBMCR',
+  },
+};
